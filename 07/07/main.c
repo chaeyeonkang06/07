3ab6c2902868ab2cb7b965485bfd2b4d490af245
chaeyeonkang06 <hellokcy@sookmyung.ac.kr>
2025-10-16 15:16:38 +0900

    1

---
+++ 07/07/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  07
+//
+//  Created by 강채연 on 10/16/25.
+//
+
+#include <stdio.h>
+
+int main(void)
+{
+    int flag = 1;
+    int y;
+    while(flag != 0)
+    {
+        y=3;
+        flag=0;
+    }
+    y=4;
+}
